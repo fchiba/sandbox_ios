7be485d4b37f6a636e87a7d2575d0d53e7cab016
Fumiya Chiba <fumiya.chiba@nifty.com>
2018-10-19 14:23:01 +0900

    Initial Commit

---
+++ sandbox/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  sandbox
+//
+//  Created by Fumiya Chiba on 2018/10/19.
+//  Copyright © 2018年 Fumiya Chiba. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ sandbox/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  sandbox
+//
+//  Created by Fumiya Chiba on 2018/10/19.
+//  Copyright © 2018年 Fumiya Chiba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
